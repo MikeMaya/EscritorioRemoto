92e83779872388c0e4c476d12b68969dc5eb6aa4
Mike <miguel.maya.27@gmail.com>
2016-05-28 20:15:58 -0500

    Update Terminal.java

--- Terminal.java
+++ Terminal.java
@@ -30,27 +30,57 @@ public class Terminal {
     private int port;
     private Scanner sc = new Scanner(System.in);
     private VideoHandler video;
-
-    public Terminal() {
-        //Do something i guess
+    private KeyboardStreamer kb;
+    private JPanel panel;
+    private JFrame mainFrame;
+    private Toolkit tool;
+    public Terminal(String host) {
+        this.host=host;
+        port=8001;
     }
-
     public void connect() {
         try {
-            getRemoto();
+            //getRemoto();
+            tool=Toolkit.getDefaultToolkit();
             Socket s=new Socket(host,port);
-            video=new VideoHandler(s, host);
+            Socket s1=new Socket(host,port+1);
+            prepareGUI();
+            video=new VideoHandler(s, host,panel);
+            kb=new KeyboardStreamer(s1,panel,video.getW(),video.getH());
             video.start();
+            kb.start();
             System.out.println("CONEXION ESTABLECIDA");
         } catch (IOException ex) {
             Logger.getLogger(Terminal.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    private void prepareGUI() {
+        mainFrame = new JFrame("Conexion remota ");
+        mainFrame.setSize(1280,720);
+        //mainFrame.setLayout(new );
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.setFocusable(true);
+        panel.requestFocusInWindow();
+        mainFrame.add(panel);
+        mainFrame.setVisible(true);
+    }
     public void getRemoto() {
         System.out.println("Ingresa la direccion a la cual se conectara");
         host = sc.nextLine();
         System.out.println("Ingresa el puerto");
         port = sc.nextInt();
+    }
+    public void detener(){
+        video.detener();
+        kb.detener();
+        video.stop();
+        kb.stop();
+        mainFrame.setVisible(false);
     }
 }
