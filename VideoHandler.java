eb197f2cca93a7876897f3b41d92337612f51447
Mike <miguel.maya.27@gmail.com>
2016-05-28 20:17:18 -0500

    Update VideoHandler.java

--- VideoHandler.java
+++ VideoHandler.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package examen2;
+package proyectoremoto;
 
 import java.awt.FlowLayout;
 import java.awt.Graphics;
@@ -12,11 +12,13 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -26,55 +28,57 @@ import javax.swing.JPanel;
  * @author MiguelAngel
  */
 class VideoHandler extends Thread {
-
+    private boolean ejecutar=true;
     private String host;
     private Socket cl;
     private Rectangle r;
     private ObjectInputStream ois;
     private JPanel panel;
-    private JFrame mainFrame;
     private Toolkit tool;
-
-    public VideoHandler(Socket c, String h) {
+    private int wR;
+    private int hR;
+    public VideoHandler(Socket c, String h, JPanel p) {
+        panel=p;
         host = h;
         tool = Toolkit.getDefaultToolkit();
         r = new Rectangle(tool.getScreenSize());
         try {
             cl = c;
             ois = new ObjectInputStream(cl.getInputStream());
+            wR=ois.readInt();
+            hR=ois.readInt();
         } catch (IOException ex) { 
             Logger.getLogger(VideoHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        prepareGUI();
     }
-
-    private void prepareGUI() {
-        mainFrame = new JFrame("Conexion remota ");
-        mainFrame.setSize(tool.getScreenSize());
-        //mainFrame.setLayout(new );
-        mainFrame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent windowEvent) {
-                System.exit(0);
-            }
-        });
-        panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        mainFrame.add(panel);
-        mainFrame.setVisible(true);
+    public int getW(){
+        return wR;
     }
-
+    public int getH(){
+        return hR;
+    }
+    
+    public void detener(){
+        try {
+            ejecutar=false;
+            ois.close();
+            cl.close();
+        } catch (IOException ex) {
+            Logger.getLogger(VideoHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     @Override
     public void run() {
-        while(true){
+        while(ejecutar){
             try {
                 ImageIcon imageIcon = (ImageIcon) ois.readObject();
+                int x=ois.readInt();
+                int y=ois.readInt();
                 Image image = imageIcon.getImage();
                 image = image.getScaledInstance(panel.getWidth(), panel.getHeight(), Image.SCALE_FAST);
                 Graphics graphics = panel.getGraphics();
                 graphics.drawImage(image, 0, 0, panel.getWidth(), panel.getHeight(), panel);
-            } catch (IOException ex) {
-                Logger.getLogger(VideoHandler.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ClassNotFoundException ex) {
+            } catch (IOException | ClassNotFoundException ex) {
                 Logger.getLogger(VideoHandler.class.getName()).log(Level.SEVERE, null, ex);
             } 
         }
