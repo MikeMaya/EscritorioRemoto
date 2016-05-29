aeeae235246f63f2202812426d075eb74d465c66
Mike <miguel.maya.27@gmail.com>
2016-05-28 20:06:59 -0500

    Update KeyboardHandler.java

--- KeyboardHandler.java
+++ KeyboardHandler.java
@@ -5,7 +5,9 @@
  */
 package proyectoremoto;
 
+import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
@@ -24,7 +26,8 @@ public class KeyboardHandler extends Thread{
         try {
             cl=c;
             ois=new ObjectInputStream(cl.getInputStream());
-        } catch (IOException ex) {
+            robot=new Robot();
+        } catch (IOException | AWTException ex) {
             Logger.getLogger(KeyboardHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -36,29 +39,42 @@ public class KeyboardHandler extends Thread{
                 switch(op){
                     case 1: //Key Pressed
                         keycode=ois.readInt();
+                        System.out.println("Key P");
                         robot.keyPress(keycode);
                         break;
                     case 2: //Key Realeased
                         keycode=ois.readInt();
-                        robot.keyPress(keycode);
+                        System.out.println("Key R");
+                        robot.keyRelease(keycode);
                         break;
                     case 3: //Mouse moved
                         x=ois.readInt();
                         y=ois.readInt();
+                        System.out.println("Mouse M");
                         robot.mouseMove(x, y);
                         break;
                     case 4: //Mouse pressed
                         keycode=ois.readInt();
-                        robot.mousePress(keycode);
+                        System.out.println("Mouse P");
+                        if(keycode==1) robot.mousePress(InputEvent.BUTTON1_MASK);
+                        else if(keycode==2) robot.mousePress(InputEvent.BUTTON2_MASK);
+                        else if(keycode==3) robot.mousePress(InputEvent.BUTTON3_MASK);
                         break;
                     case 5: //Mouse realeased
                         keycode=ois.readInt();
-                        robot.mouseRelease(keycode);
+                        System.out.println("Mouse R");
+                        if(keycode==1) robot.mouseRelease(InputEvent.BUTTON1_MASK);
+                        else if(keycode==2) robot.mouseRelease(InputEvent.BUTTON2_MASK);
+                        else if(keycode==3) robot.mouseRelease(InputEvent.BUTTON3_MASK);
                         break;
+                    case 6:
+                        ois.close();
+                        cl.close();
+                        return;
                 }
             } catch (IOException ex) {
                 Logger.getLogger(KeyboardHandler.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            } 
         }
     }
 }
